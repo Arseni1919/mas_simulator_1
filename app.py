4601809dbebb7dae3f17ae236e93cf36a828af84
perchik_air <1919ars@gmail.com>
2021-12-30 14:11:44 +0200

    (air)

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ def main():
 
 
 if __name__ == '__main__':
+    # parallel_env = pistonball_v4.parallel_env()
+    # observations = parallel_env.reset()
+    # max_cycles = 500
+    # for step in range(max_cycles):
+    #     actions = {agent: policy(observations[agent], agent) for agent in parallel_env.agents}
+    #     observations, rewards, dones, infos = parallel_env.step(actions)
     main()
 
 
--- simulator/msa_simulator.py
+++ simulator/msa_simulator.py
@@ -12,7 +12,7 @@ Methods:
     action_space(agent)
     
     step(actions): receives a dictionary of actions keyed by the agent name. Returns the observation dictionary, 
-    reward dictionary, done dictionary, and info dictionary, where each dictionary is keyed by the agent.
+                   reward dictionary, done dictionary, and info dictionary, where each dictionary is keyed by the agent.
     
     reset(): resets the environment and returns a dictionary of observations (keyed by the agent name)
 
@@ -20,3 +20,59 @@ Functions:
     random_demo(env, render=True, episodes=1)
 
 """
+
+
+class MSASimulator:
+
+    def __init__(self):
+        agents = None
+        num_agents = None
+        possible_agents = None
+        max_num_agents = None
+        observation_spaces = None
+        action_spaces = None
+
+    def seed(self):
+        pass
+
+    def observation_space(self, agent):
+        pass
+
+    def action_space(self, agent):
+        pass
+
+    def step(self, actions):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self):
+        pass
+
+    def random_demo(self, render=True, episodes=1):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
